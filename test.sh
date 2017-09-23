d47d7301dfb4d97da1a82f91daa762b45dcb549e
Nenad Kuzmanovic <kuzma86@gmail.com>
2017-09-23 23:08:39 +0200

    Added tag push to script

--- test.sh
+++ test.sh
@@ -2,24 +2,52 @@
 
 version=$1
 
-if [ -z "$version"]
+if [ -z "$version" ]
 then
   echo Mising version argument
-  exit 1
+  exit
 fi
 
 echo Checking if tag $version exists...
-if git tag --list | egrep $1 
+
+git fetch
+
+if git tag --list | egrep $version
 then
   echo Tag $version already exist
 else
-  git checkout develop
-  git pull
-  git checkout master
-  git pull
-  git merge develop
-fi
+  echo Checking out develop...
+  if [[ ! $(git checkout develop) ]]; then
+    echo Can\'t checkout develop
+    exit
+  else 
+    echo Pulling develop...
+    if [[ ! $(git pull) ]]; then
+      echo Can\'t pull develop
+      exit
+    else 
+    echo Checking out master...
+    if [[ ! $(git checkout master) ]]; then
+      echo Can\'t checkout master
+      exit
+    else 
+    echo Pulling master...
+    if [[ ! $(git pull) ]]; then
+      echo Can\'t pull master
+      exit
+    else 
+    echo Merging develop-\>master... 
+    if  [[ ! $(git merge develop) ]]; then
+      echo Can\'t merge develop-\>master
+      exit
+    else
+    echo Creating tag $version ...
+    git tag -a $version -m '$version'
+    echo Pushing tag to origin... 
+    git push origin $version
 
+  fi fi fi fi fi
+fi
 
 echo done
 
